31969dc165281483c82f97b5dfc9e5440a263e85
Manoj <padigineti.manojkumar@gmail.com>
2025-06-03 20:52:02 +0530

    Added build groovy pipeline

--- vars/docker_pipeline.groovy
+++ vars/docker_pipeline.groovy
@@ -2,7 +2,9 @@ def call ( map config ) {
   def appName = config.appName
 node ('agent1') {
     stage('build ${appName}') {
-       mvn --version
+      sh """"
+        mvn --version
+      """"
     }
 }
 }
