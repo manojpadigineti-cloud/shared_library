2015191aa57f8883ae352984732f13f268d41311
Manoj <padigineti.manojkumar@gmail.com>
2025-06-04 22:54:30 +0530

    Added build groovy pipeline

--- vars/docker_pipeline.groovy
+++ vars/docker_pipeline.groovy
@@ -1,21 +1,27 @@
 def call ( Map config ) {
     properties([
         parameters([
-            choice(name: 'DEPLOY_ENV', choices: ['Dev', 'QA', 'Stage', 'Prod'], description: 'Where to deploy the application')
+            choice(name: 'Code_Build', choices: ['NO', 'YES'], description: 'Building your code required')
+            choice(name: 'DEPLOY_ENV', choices: ['N/A','Dev', 'QA', 'Stage', 'Prod'], description: 'Where to deploy the application')
         ])
     ])
-  def appName = config.appName
     node ('agent1') {
+     env.appName = config.appName
         stage("checkout SCM") {
         withCredentials([gitUsernamePassword(credentialsId: 'Github_Token_New', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
            checkout scm
          }
         }
-        stage("build ${appName}") {
-          sh """
-            mvn clean package -DskipTests
-          """
-        stage ("Code Scan for ${appName}") {
+        stage("build ${env.appName}") {
+          script {
+          if (params.Code_Build == 'yes') {
+            sh 'mvn clean package -DskipTests'
+            }
+          else {
+            echo "Skipping the build as ${params.Code_Build}"
+            }
+          }
+        stage ("Code Scan for ${env.appName}") {
             sh """
             echo "Sonar Scan Stage"
             """
