f8fde51c09c3acdec7970579fd879629c206d770
Manoj <padigineti.manojkumar@gmail.com>
2025-06-04 22:37:26 +0530

    Added build groovy pipeline

--- vars/docker_pipeline.groovy
+++ vars/docker_pipeline.groovy
@@ -5,19 +5,16 @@ def call ( Map config ) {
     )]
   )]
   def appName = config.appName
-  def gitrepo = "https://github.com/manojpadigineti-cloud/i27-eureka.git"
-  pipeline {
-node ('agent1') {
-    stage("checkout SCM") {
-    withCredentials([gitUsernamePassword(credentialsId: 'Github_Token_New', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-       checkout scm
+    node ('agent1') {
+        stage("checkout SCM") {
+        withCredentials([gitUsernamePassword(credentialsId: 'Github_Token_New', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+           checkout scm
+         }
+        }
+        stage("build ${appName}") {
+          sh """
+            mvn clean package -D maven.test.skip=true
+          """
+        }
      }
-    }
-    stage("build ${appName}") {
-      sh """
-        mvn clean package -D maven.test.skip=true
-      """
-    }
-}
-}
-}
+ }
